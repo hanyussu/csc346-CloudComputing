ebe3057c0e4eac411bcb84d954cd00f43269facc
Han Yu Su <hanyusu@arizona.edu>
2025-03-03 13:01:50 -0700

    Add my script to use passwords.py for authentication

---
+++ activity7_2.py
@@ -0,0 +1,8 @@
+import passwords
+
+username = passwords.USERNAME
+password = passwords.PASSWORD
+
+# Use the username and password for authentication
+print(f"Username: {username}")
+print(f"Password: {password}")
